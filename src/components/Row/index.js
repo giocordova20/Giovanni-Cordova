16622d804f37773d8797f607616dec8312bf348e
giocode <giocordova@gmail.com>
2021-02-02 01:33:01 -0500

    removed console.log

--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 function Row(props) {
-  console.log({props})
-  return <div className="row" style={props.style}>{props.children} </div>;
+  return <div className="row" style={props.style}>
+      {props.children}
+    </div>;
 }
 
 export default Row;
